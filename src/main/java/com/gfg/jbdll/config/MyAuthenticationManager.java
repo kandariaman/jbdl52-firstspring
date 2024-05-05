92b8b263b49d4e998f6685d7d8d7ced6161bb3b9
Aman Kandari <kandariaman24@gmail.com>
2024-05-06 01:18:39 +0530

    Writing the authentication manager

--- src/main/java/com/gfg/jbdll/config/MyAuthenticationManager.java
+++ src/main/java/com/gfg/jbdll/config/MyAuthenticationManager.java
@@ -1,19 +1,43 @@
 package com.gfg.jbdll.config;
 
+import com.gfg.jbdll.service.MyUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class MyAuthenticationManager implements AuthenticationProvider {
 
+
+    @Autowired
+    MyUserDetailService userDetailsService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return null;
+        String name = authentication.getName();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(name);
+
+        if(passwordEncoder.matches(userDetails.getPassword(), authentication.getCredentials().toString()))
+            return new UsernamePasswordAuthenticationToken(authentication.getPrincipal(), authentication.getCredentials(), userDetails.getAuthorities());
+
+        else
+            throw new BadCredentialsException("Invalid credentials");
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return false;
+        if(authentication.getClass().equals(UsernamePasswordAuthenticationToken.class))
+            return true;
+        else
+            return false;
     }
 }
